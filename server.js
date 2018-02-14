75db51a4d300e5abee85f2ba639862e66d8eccc1
DavidWei95036593 <dw36593@pausd.us>
2018-02-14 13:21:12 -0800

    Make three pages

--- server.js
+++ server.js
@@ -3,33 +3,42 @@ const hbs = require(`hbs`);
 const path = require(`path`);
 // require('./public/css/style.css');
 const app= express();
+
+//this make anything in public to be avaible to the js
+app.use(express.static(path.join(__dirname,"public","css")));
+
 app.set(`view engine`,`hbs`);
 hbs.registerPartials(path.join(__dirname,"view","partials"))
 app.get(`/`,(req,res)=>{
+  // res.redirect(`www.paly.net`);
   res.render(`index.hbs`,{
-    about:`About Me`,
+    CityName:`Choose Your City`,
     content:`Jeremy is a despacito`,
     school:`Paly`,
     pageTitle:`Home`
   });
+app.get(`/losangelos`,(req,res)=>{
+  res.render(`index.hbs`,{
+    CityName:`Los Angelos`
+  })
+});
+app.get(`/sanantonio`,(req,res)=>{
+  res.render(`index.hbs`,{
+    CityName:`San Antonio`
+  })
+});
+app.get(`/chicago`,(req,res)=>{
+  res.render(`index.hbs`,{
+    CityName:`Chicago`
+  })
+});
 //sentfile sent it to the excate location
 // app.sendFile(__dirname+"/public");
 })
 
-app.get(`/main.css`,(req,res)=>{
-  res.render(`main.css`,{
-    // p{color:red}
-  });
-})
+
 
 //methods
-
-
-
-
-
-
-
 
 
 app.listen(3000,()=>{
