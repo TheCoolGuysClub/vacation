8e27a8c578c4d9f3febbd4d866040d7b97c70d50
DavidWei95036593 <dw36593@pausd.us>
2018-02-15 09:27:33 -0800

    nearly finish

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
+const weather = require(`./views/weather/weather.js`);
 const express = require(`express`);
 const hbs = require(`hbs`);
 const path = require(`path`);
-
 const app= express();
 
 //this make anything in public to be avaible to the js
 app.use(express.static(path.join(__dirname,"public","css")));
 
 app.set(`view engine`,`hbs`);
-hbs.registerPartials(path.join(__dirname,"view","partials"))
+hbs.registerPartials(path.join(__dirname,"view","partials"));
+hbs.registerHelper(`getCurrentYear`,()=>{
+  return new Date().getFullYear();
+})
+hbs.registerHelper(`weatherResults`,(lag,lng)=>{
+  return weather.weatherResults(lag,lng);
+})
 app.get(`/`,(req,res)=>{
   // res.redirect(`www.paly.net`);
   res.render(`index.hbs`,{
+      temperature:0,
     imgone:`https://mitechnews.com/wp-content/uploads/2017/08/on-vacation.jpg`,
     imgtwo:`http://i2.cdn.turner.com/money/dam/assets/140730120927-employee-mandatory-vacation-1024x576.jpg`,
     imgthree:`https://www.lachicotte.com/images/rotate/vacation-rentals-slider3.jpg`,
@@ -21,8 +28,11 @@ app.get(`/`,(req,res)=>{
     pageTitle:`Home`
   });
 app.get(`/losangelos`,(req,res)=>{
-  temperature:`3000`,
+
   res.render(`index.hbs`,{
+    temperature:weather.weatherResults(34.0522342,-118.2436849),
+    lag:`34.0522342`,
+    lng:`-118.2436849`,
     CityName:`Los Angelos`,
     imgone:`http://travel.home.sndimg.com/content/dam/images/travel/fullset/2011/09/12/af/chicago-lake-shore-drive.rend.hgtvcom.616.462.suffix/1491585454223.jpeg`,
     imgtwo:`http://vacationpackagesallinclusiv-e.com/wp-content/uploads/2012/10/Chicago.jpg`,
@@ -31,7 +41,9 @@ app.get(`/losangelos`,(req,res)=>{
 });
 app.get(`/sanantonio`,(req,res)=>{
   res.render(`index.hbs`,{
-    temperature:`3000`,
+    lag:`29.4241219`,
+    lng:`-98.49362819999999`,
+      temperature:weather.weatherResults(29.4241219,-98.49362819999999),
     CityName:`San Antonio`,
     imgone:`https://www.chcp.edu/sites/default/files/CHCP-San-Antonio-Campus.jpg`,
     imgtwo:`https://cache-graphicslib.viator.com/graphicslib/thumbs360x240/10152/SITours/san-antonio-super-pass-in-san-antonio-424265.jpg`,
@@ -40,7 +52,9 @@ app.get(`/sanantonio`,(req,res)=>{
 });
 app.get(`/chicago`,(req,res)=>{
   res.render(`index.hbs`,{
-    temperature:`3000`,
+    lag:`41.8781136`,
+    lng:`-87.6297982`,
+    temperature:weather.weatherResults(41.8781136,-87.6297982),
     CityName:`Chicago`,
     imgone:`https://urbanmatter.com/chicago/wp-content/uploads/2015/04/Chicago-Architecture.jpg`,
     imgtwo:`http://www.essexinn.com/d/essexinn/media/Attractions/3583017878_ee2e8e3d8b_b_1423674445483.jpg`,
@@ -57,5 +71,7 @@ app.get(`/chicago`,(req,res)=>{
 
 
 app.listen(3000,()=>{
+    // weather.weatherResults(41.8781136,-87.6297982);
+  // console.log("lo");
 console.log("Listening on port 3000!");
 })
--- views/weather/weather.js
+++ views/weather/weather.js
@@ -1,13 +1,6 @@
 const request = require('request');
-
 const weatherResults=(lag,long,callback)=>{
-  // location: {
-  // lat: 37.4357409,
-  // lng: -122.1556904
-  // },
-
   request({
-
     url:`https://api.darksky.net/forecast/7fa261da4fc6cad0aef6384618ce197e/${lag},${long}`,
     json:true
     },
@@ -24,23 +17,19 @@ const weatherResults=(lag,long,callback)=>{
         const min = new Date().getMinutes();
         const sec = new Date().getSeconds();
         const icon = body.currently.icon;
-        console.log(" (╯°□°）╯︵ ┻━┻");
+        // console.log(" (╯°□°）╯︵ ┻━┻");
         console.log(`The temperature is ${temperature} degrees, for me, it feels like ${apperntTemperature} degrees`);
         console.log(`the visibility is ${visibility}`);
         console.log(`the nearest storm is about ${nearestSotrmDistance} miles away`);
         console.log(`The current date is ${month}/${day}/${year},the time is ${hour}:${min}:${sec}`);
-        console.log("┻━┻ミ＼(≧ﾛ≦＼)");
         console.log(`The weather is ${icon}`);
         // console.log(`The sun is up for ${sunriseTime} seconds! And will set in ${sunsetTime} seconds!`);
+        return temperature;
       }
   else {
         console.log("Unable to fetch temperature");
       }
   })
-  //
-
-
-
 }
 module.exports={
   weatherResults
