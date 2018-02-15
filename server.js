61b9b1cc176952d07665f6aee65d79979786e5a0
DavidWei95036593 <dw36593@pausd.us>
2018-02-15 15:07:02 -0800

    finish the whole thing, the only thing to do is better styleing

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const weather = require(`./views/weather/weather.js`);
 const express = require(`express`);
 const hbs = require(`hbs`);
 const path = require(`path`);
+const axios = require(`axios`);
 const app= express();
 
 //this make anything in public to be avaible to the js
@@ -26,37 +27,46 @@ app.get(`/`,(req,res)=>{
 
   });
 app.get(`/losangelos`,(req,res)=>{
-
-  res.render(`index.hbs`,{
-    temperature:`The temperature of Los Angelos is ` + `${weather.weatherResults(34.0522342,-118.2436849)}` +` degress!`,
-    lag:`34.0522342`,
-    lng:`-118.2436849`,
-    CityName:`Los Angelos`,
-    imgone:`http://travel.home.sndimg.com/content/dam/images/travel/fullset/2011/09/12/af/chicago-lake-shore-drive.rend.hgtvcom.616.462.suffix/1491585454223.jpeg`,
-    imgtwo:`http://vacationpackagesallinclusiv-e.com/wp-content/uploads/2012/10/Chicago.jpg`,
-    imgthree:`http://athomemms.com/index/wp-content/uploads/2014/03/What-to-do-in-Chicago-IL-fun-family-vacation-ideas-.jpg`
-  })
+  const lat =`34.0522342`;
+  const lng =`-118.2436849`;
+  weather.weatherResults(lat,lng)
+  .then((response) => {
+      res.render(`index.hbs`,{
+        temperature:`The temperature of Los Angelos is ${response.data.currently.temperature} degress!`,
+        CityName:`Los Angelos`,
+        imgone:`http://travel.home.sndimg.com/content/dam/images/travel/fullset/2011/09/12/af/chicago-lake-shore-drive.rend.hgtvcom.616.462.suffix/1491585454223.jpeg`,
+        imgtwo:`http://vacationpackagesallinclusiv-e.com/wp-content/uploads/2012/10/Chicago.jpg`,
+        imgthree:`http://athomemms.com/index/wp-content/uploads/2014/03/What-to-do-in-Chicago-IL-fun-family-vacation-ideas-.jpg`
+      })
+    })
 });
 app.get(`/sanantonio`,(req,res)=>{
-  res.render(`index.hbs`,{
-    lag:`29.4241219`,
-    lng:`-98.49362819999999`,
-    temperature:`The temperature of San Antonio is ` + `${weather.weatherResults(29.4241219,-98.49362819999999)}` +` degress! `,
+  const lat = `29.4241219`;
+  const lng = `-98.49362819999999`;
+  weather.weatherResults(lat,lng)
+  .then((response)=>{res.render(`index.hbs`,{
+    temperature:`The temperature of San Antonio is ${response.data.currently.temperature} degress! `,
     CityName:`San Antonio`,
     imgone:`https://www.chcp.edu/sites/default/files/CHCP-San-Antonio-Campus.jpg`,
     imgtwo:`https://cache-graphicslib.viator.com/graphicslib/thumbs360x240/10152/SITours/san-antonio-super-pass-in-san-antonio-424265.jpg`,
     imgthree:`http://www.planetware.com/photos-large/USTX/texas-san-antonio-things-to-do-the-saga.jpg`
   })
+})
+
+
 });
 app.get(`/chicago`,(req,res)=>{
-  res.render(`index.hbs`,{
-    lag:`41.8781136`,
-    lng:`-87.6297982`,
-    temperature:`The temperature of Chicago is ` + `${weather.weatherResults(41.8781136,-87.6297982)}` +` degress!` ,
-    CityName:`Chicago`,
-    imgone:`https://urbanmatter.com/chicago/wp-content/uploads/2015/04/Chicago-Architecture.jpg`,
-    imgtwo:`http://www.essexinn.com/d/essexinn/media/Attractions/3583017878_ee2e8e3d8b_b_1423674445483.jpg`,
-    imgthree:`https://i.kinja-img.com/gawker-media/image/upload/s--T0-K6gLo--/c_scale,fl_progressive,q_80,w_800/wtcnqekq9zwta5gpq48r.jpg`
+  const lat=`41.8781136`;
+  const lng = `-87.6297982`;
+  weather.weatherResults(lat,lng)
+  .then((response)=>{
+    res.render(`index.hbs`,{
+      temperature:`The temperature of Chicago is ${response.data.currently.temperature} degress!` ,
+      CityName:`Chicago`,
+      imgone:`https://urbanmatter.com/chicago/wp-content/uploads/2015/04/Chicago-Architecture.jpg`,
+      imgtwo:`http://www.essexinn.com/d/essexinn/media/Attractions/3583017878_ee2e8e3d8b_b_1423674445483.jpg`,
+      imgthree:`https://i.kinja-img.com/gawker-media/image/upload/s--T0-K6gLo--/c_scale,fl_progressive,q_80,w_800/wtcnqekq9zwta5gpq48r.jpg`
+    })
   })
 });
 //sentfile sent it to the excate location
--- views/weather/weather.js
+++ views/weather/weather.js
@@ -1,38 +1,31 @@
-const request = require('request');
-const weatherResults=(lag,long,callback)=>{
-  request({
-    url:`https://api.darksky.net/forecast/7fa261da4fc6cad0aef6384618ce197e/${lag},${long}`,
-    json:true
-    },
-    (error,response,body)=>{
-      if (!error &&body.code != 400){
-        const temperature = body.currently.temperature;
-        const apperntTemperature = body.currently.apparentTemperature;
-        const visibility = body.currently.visibility;
-        const nearestSotrmDistance = body.currently.nearestStormDistance;
-        const year = new Date().getFullYear();
-        const month = new Date().getMonth()+1;
-        const day = new Date().getDate();
-        const hour = new Date().getHours();
-        const min = new Date().getMinutes();
-        const sec = new Date().getSeconds();
-        const icon = body.currently.icon;
-        console.log(`${temperature}`);
-        // console.log(" (╯°□°）╯︵ ┻━┻");
-        // console.log(`The temperature is ${temperature} degrees, for me, it feels like ${apperntTemperature} degrees`);
-        // console.log(`the visibility is ${visibility}`);
-        // console.log(`the nearest storm is about ${nearestSotrmDistance} miles away`);
-        // console.log(`The current date is ${month}/${day}/${year},the time is ${hour}:${min}:${sec}`);
-        // console.log(`The weather is ${icon}`);
-        // console.log(`The sun is up for ${sunriseTime} seconds! And will set in ${sunsetTime} seconds!`);
-        return `hi`;
-      }
-  else {
-        console.log("Unable to fetch temperature");
-      }
-      return "ih";
-  })
-}
-module.exports={
+const axios = require(`axios`);
+const weatherResults = (lag,lng)=>{
+      let temperature = null;
+        const wethercodeURL = `https://api.darksky.net/forecast/7fa261da4fc6cad0aef6384618ce197e/${lag},${lng}`;
+        return axios.get(wethercodeURL);
+        // .then((response)=>{
+        //     const timezone = response.data.timezone;
+        //     temperature = response.data.currently.temperature;
+        //     const aptemperature = response.data.currently.apparentTemperature;
+        //     const visibility = response.data.currently.visibility;
+        //     const year = new Date().getFullYear();
+        //     const month = new Date().getMonth()+1;
+        //     const day = new Date().getDate();
+        //     const hour = new Date().getHours();
+        //     const min = new Date().getMinutes();
+        //     const sec = new Date().getSeconds();
+            // console.log(`Your Time zone is ${timezone}`);
+            // console.log( `The temperature is ${temperature},and it feels like ${aptemperature}`);
+            // console.log(`The visibility is ${visibility}`);
+            // console.log(`The current date is ${month}/${day}/${year},the time is ${hour}:${min}:${sec}`);
+            // console.log("┻━┻ミ＼(≧ﾛ≦＼)");
+            // console.log(`The weather is ${icon}`);
+            // console.log(temperature);
+          // })
+        // console.log(temperature);
+        // return temperature;
+
+  }
+module.exports = {
   weatherResults
 }
